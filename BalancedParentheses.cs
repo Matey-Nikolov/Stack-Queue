50e34f6800d9075c6029c704cb1cfd53e9590c51
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2021-06-15 19:35:11 +0300

    Add files via upload

---
+++ BalancedParentheses.cs
@@ -0,0 +1,42 @@
+﻿namespace BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            if (!(1 <= input.Length && input.Length <= 1000))
+            {
+                Console.WriteLine("NO");
+            }
+
+            Stack<char> openParenthesis = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (currentChar == '{' || currentChar == '(' || currentChar == '[')
+                    openParenthesis.Push(currentChar);
+
+                char peekParenthesi = openParenthesis.Peek();
+
+                if (peekParenthesi == '(' && currentChar == ')')
+                    openParenthesis.Pop();
+                else if (peekParenthesi == '[' && currentChar == ']')
+                    openParenthesis.Pop();
+                else if (peekParenthesi == '{' && currentChar == '}')
+                    openParenthesis.Pop();
+            }
+
+            if (openParenthesis.Count == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
---
+++ SimpleTextEditor.cs
@@ -0,0 +1,53 @@
+﻿namespace SimpleTextEditor
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (!(1 <= n && n <= 105))
+            {
+                return;
+            }
+
+            Stack<string> text = new Stack<string>();
+            string currentTextAndNumber = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int command = int.Parse(input[0]);
+                string textOrNumber = "";
+                if (input.Length == 2)
+                    textOrNumber = input[1];
+
+                switch (command)
+                {
+                    case 1:
+                        text.Push(textOrNumber);
+                        currentTextAndNumber += textOrNumber;
+                        break;
+                    case 2:
+                        text.Push(currentTextAndNumber);
+                        int num = int.Parse(textOrNumber);
+                        currentTextAndNumber = currentTextAndNumber
+                            .Substring(0, currentTextAndNumber.Length - num);
+                        break;
+                    case 3:
+                        int printIndexChar = currentTextAndNumber[int.Parse(textOrNumber) - 1];
+                        Console.WriteLine(((char)printIndexChar));
+                        break;
+                    case 4:
+                        currentTextAndNumber = text.Pop();
+                        break;
+                }
+            }
+        }
+    }
+}
